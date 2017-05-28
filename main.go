28dddc8caeb7c310de22f488af209cbdd9e0b42e
nbari <nbari@tequila.io>
2017-05-28 18:18:51 +0200

    test seting headers on request and response

--- main.go
+++ main.go
@@ -8,7 +8,8 @@ import (
 func WAF(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		log.Println("WAF init")
-		w.Header().Set("X-WAF-Version", "1.0")
+		w.Header().Set("sq-waf-version", "1.0.0")
+		r.Header.Set("sq-waf-version", "1.0.0")
 		next.ServeHTTP(w, r)
 	})
 }
