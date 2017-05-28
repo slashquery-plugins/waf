da1dbeaafd0422821dbcf210b08159eaa1f97a4b
nbari <nbari@tequila.io>
2017-05-28 16:34:31 +0200

    write header

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 func WAF(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		log.Println("WAF init")
+		w.Header().Set("X-WAF-Version", "1.0")
 		next.ServeHTTP(w, r)
 	})
 }
