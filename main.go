003a10ff5c814a5044d4958bd4a0a2f2a6abb4a1
nbari <nbari@tequila.io>
2017-05-31 01:17:31 +0200

    sync

--- main.go
+++ main.go
@@ -3,13 +3,15 @@ package waf
 import (
 	"log"
 	"net/http"
+	"time"
 )
 
 func WAF(next http.Handler) http.Handler {
+	time := time.Now().String()
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		log.Println("WAF init")
-		w.Header().Set("sq-waf-version", "1.0.0")
-		r.Header.Set("sq-waf-version", "1.0.0")
+		w.Header().Set("sq-waf-version", time)
+		r.Header.Set("sq-waf-version", time)
 		next.ServeHTTP(w, r)
 	})
 }
